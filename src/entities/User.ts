b803cbc16dd3a488389fc2995c3d782962f9a5dd
Marcos Maverick <marquinhopimpo@gmail.com>
2021-06-24 12:14:22 -0300

    first commit

---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+class User {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+
+export { User }
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata';
+import express, { request } from 'express'
+
+import './database'
+
+const app = express()
+
+app.listen(3000, () => console.log('server is running...'))
