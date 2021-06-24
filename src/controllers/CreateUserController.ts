3f98aec444c7a2bc1ad58fe4cef8f01dd605e53f
Marcos Maverick <marquinhopimpo@gmail.com>
2021-06-24 13:44:30 -0300

    chore: add Controllers

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+
+class CreateUserController {
+
+    async handle(request: Request, response: Response) {
+        const { } =
+    }
+}
+
+export { CreateUserController }
